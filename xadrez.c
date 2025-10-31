a9968852eed3a74b706162e3e34f33093f04b9bc
Gustavo Santana <115935313+Gusttavo-KaZuKi@users.noreply.github.com>
2025-10-31 18:01:24 -0300

    Adicionar lógica de movimento das peças do Xadrez

--- xadrez.c
+++ xadrez.c
@@ -5,28 +5,28 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    int torre = 0, bispo = 0, rainha = 0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    // Move a rainha 8 casas para a esquerda
+    for (rainha; rainha < 8; rainha++) {
+        printf("Esquerda\n");
+    }
+    printf("Rainha se moveu 8 casas para a esquerda\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    // Move o bispo 5 casas na diagonal para cima à direita
+    do {
+        printf("Cima Direita\n");
+        bispo++;
+    } while (bispo < 5);
+    printf("Bispo se moveu 5 casas na diagonal para cima à direita\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    // Move a torre 5 casas para a direita
+    while (torre < 5) {
+        printf("Direita\n");
+        torre++;
+    }
+    printf("Torre se moveu 5 casas para a direita\n");
 
     return 0;
 }
+
